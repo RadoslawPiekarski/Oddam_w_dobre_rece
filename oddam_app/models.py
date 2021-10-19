3513050e1afb8e0ff0187e7d09ff26d932592b81
Radek <radoslaw.piekarski@gmail.com>
2021-10-20 00:34:31 +0200

    added Donation model

--- oddam_app/models.py
+++ oddam_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,3 +18,16 @@ class Institution(models.Model):
     description = models.TextField
     type = models.CharField(max_length=1, choices=TYPES, default='F')
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    institution = models.ForeignKey(Institution, null=True, on_delete=models.SET_NULL)
+    address = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=120)
+    user = models.OneToOneField(User, null=True, default=True, on_delete=models.SET_NULL)
