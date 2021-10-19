aec9f88600fcb5eac046f0c57f61d49e8b4ab86c
Radek <radoslaw.piekarski@gmail.com>
2021-10-19 21:43:15 +0200

    added simplified LandingPage class view

--- oddam_app/views.py
+++ oddam_app/views.py
@@ -3,11 +3,15 @@ from django.views import View
 
 # Create your views here.
 
+
 class LandingPage(View):
     def get(self, request):
         return render(request, "oddam_app/index.html")
 
 
+class AddDonation(View):
+    def get(self, request):
+        return render(request, "oddam_app/form.html")
 # AddDonation
 # Login
 # Register
