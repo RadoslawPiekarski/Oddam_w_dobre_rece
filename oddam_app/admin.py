9b042288802cb51937cacd3b8609fd05cb878a2f
Radek <radoslaw.piekarski@gmail.com>
2021-10-20 13:07:18 +0200

    registered models in admin

--- oddam_app/admin.py
+++ oddam_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Institution, Donation, Category
 # Register your models here.
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_filter = ("type",)
+    list_display = ("name", "type",)
+
+
+class DonationAdmin(admin.ModelAdmin):
+    list_filter = ("user", "institution", "city", )
+    list_display = ("user", "city", "quantity", "institution")
+
+
+admin.site.register(Institution, InstitutionAdmin)
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(Category)
