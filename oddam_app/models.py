784e600e6908dcbdfb551dbb71fbb28d1414d04e
Radek <radoslaw.piekarski@gmail.com>
2021-10-20 13:19:22 +0200

    added str repr for Category model

--- oddam_app/models.py
+++ oddam_app/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
 
 class Institution(models.Model):
     TYPES = (
