1528fb8e84a30bcaee316b08593088e2e8d7e4c4
Radek <radoslaw.piekarski@gmail.com>
2021-10-21 14:34:21 +0200

    deleted 'debigging' prints

--- oddam_app/views.py
+++ oddam_app/views.py
@@ -10,7 +10,6 @@ class LandingPage(View):
     def get(self, request):
         all_given_donations = list(Donation.objects.aggregate(Sum('quantity')).values())[0]
         all_donated_organisations = Donation.objects.values('institution').count()
-        print(all_donated_organisations)
         return render(request, "oddam_app/index.html", {
             "all_given_donations": all_given_donations,
             "all_donated_organisations": all_donated_organisations,
