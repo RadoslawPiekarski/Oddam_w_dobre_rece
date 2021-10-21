c89ded1fc0f119506dd8d64e6dd58036ffce4634
Radek <radoslaw.piekarski@gmail.com>
2021-10-21 14:30:26 +0200

    take all donated organisations from database

--- oddam_app/views.py
+++ oddam_app/views.py
@@ -9,7 +9,8 @@ from .models import Donation
 class LandingPage(View):
     def get(self, request):
         all_given_donations = list(Donation.objects.aggregate(Sum('quantity')).values())[0]
-        print(all_given_donations)
+        all_donated_organisations = Donation.objects.values('institution').count()
+        print(all_donated_organisations)
         return render(request, "oddam_app/index.html", {
             "all_given_donations": all_given_donations,
         })
