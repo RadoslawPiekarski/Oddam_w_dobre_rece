199bf456ba1f1129a4b18ea63d4e77d8080bfac0
Radek <radoslaw.piekarski@gmail.com>
2021-10-21 14:15:46 +0200

    take all given donations from database

--- oddam_app/views.py
+++ oddam_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Sum
+from .models import Donation
 
 # Create your views here.
 
 
 class LandingPage(View):
     def get(self, request):
+        all_given_donations = list(Donation.objects.aggregate(Sum('quantity')).values())[0]
+        print(all_given_donations)
         return render(request, "oddam_app/index.html")
 
 
