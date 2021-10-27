5213dacdacbe2b3a9ce57dc931346e1132d597f2
Radek <radoslaw.piekarski@gmail.com>
2021-10-27 22:59:12 +0200

    deleted empty lines

--- oddam_app/forms.py
+++ oddam_app/forms.py
@@ -10,18 +10,3 @@ class CreateUserForm(forms.Form):
     firstname = forms.CharField(max_length=20, label="Imię", widget=forms.TextInput(attrs={'placeholder': 'Imię'}))
     lastname = forms.CharField(max_length=50, label="Nazwisko", widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
     email = forms.EmailField(max_length=50, label="E-mail", widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-
-
-# class NewUserForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-#
-#     class Meta:
-#         model = User
-#         fields = ('username', 'email', 'password1', 'password2')
-#
-#     def save(self, commit=True):
-#         user = super(NewUserForm, self).save(commit=False)
-#         user.email = self.cleaned_data['email']
-#         if commit:
-#             user.save()
-#         return user
