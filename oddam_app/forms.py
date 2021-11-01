9237de3a9e977234e779f1507cc82a2329fde00b
Radek <radoslaw.piekarski@gmail.com>
2021-11-01 12:20:48 +0100

    changed username to email

--- oddam_app/forms.py
+++ oddam_app/forms.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 
 
 class CreateUserForm(forms.Form):
-    username = forms.CharField(max_length=20, label="Login", widget=forms.TextInput(attrs={'placeholder': 'Login'}))
+    # username = forms.CharField(max_length=20, label="E-mail", widget=forms.TextInput(attrs={'placeholder': 'Login'}))
+    email = forms.EmailField(max_length=50, label="E-mail", widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     password = forms.CharField(max_length=20, label="Hasło", widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'Hasło'}))
     password2 = forms.CharField(max_length=20, label="Powtórz hasło", widget=forms.PasswordInput(render_value=False, attrs={'placeholder': 'Powtórz hasło'}))
     firstname = forms.CharField(max_length=20, label="Imię", widget=forms.TextInput(attrs={'placeholder': 'Imię'}))
     lastname = forms.CharField(max_length=50, label="Nazwisko", widget=forms.TextInput(attrs={'placeholder': 'Nazwisko'}))
-    email = forms.EmailField(max_length=50, label="E-mail", widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+
